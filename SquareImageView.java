44a48815411bfd8e63fd5a19dc7e16cb4fcb87fa
ykai <yangyankai_mr@outlook.com>
2018-11-04 18:19:41 +0800

    Create SquareImageView.java

---
+++ SquareImageView.java
@@ -0,0 +1,26 @@
+package test.ykai.com.app_test.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+// 自定义View,实现一个宽高相等的imageview.
+
+public class SquareImageView extends ImageView {
+
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
